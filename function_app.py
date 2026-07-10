74332da563a47e3bf9d014e2d65d908245ee76f4
Greg Wu <wugreg@microsoft.com>
2026-07-09 21:33:48 -0700

    initial checkin

---
+++ function_app.py
@@ -0,0 +1,28 @@
+import azure.functions as func
+import logging
+import sys
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="http_trigger")
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    python_version = sys.version
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully. Python version: {python_version}")
+    else:
+        return func.HttpResponse(
+             f"This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response. Python version: {python_version}",
+             status_code=200
+        )
