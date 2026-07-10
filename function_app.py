91172aa7471765646d6ee3182cd8fc310ccedd0d
Greg Wu <wugreg@microsoft.com>
2026-07-09 22:23:00 -0700

    minor update to trigger deploy

--- function_app.py
+++ function_app.py
@@ -23,6 +23,6 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully. Python version: {python_version}")
     else:
         return func.HttpResponse(
-             f"This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response. Python version: {python_version}",
+             f"This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response. Python version: {python_version} gwuflexpy",
              status_code=200
         )
